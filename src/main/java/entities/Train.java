70156b5cb2a60858ce83d6bfa10189df6140e7d3
jurciusmantas <jurciusmantas@gmail.com>
2020-04-07 21:04:09 +0300

    finally something working

---
+++ src/main/java/entities/Train.java
@@ -0,0 +1,41 @@
+package entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@NamedQueries({
+        @NamedQuery(name = "Trains.findAll", query = "select t from Train as t")
+})
+@Table(name = "TRAIN")
+@Getter @Setter
+public class Train implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Size(max = 50)
+    @Column(name = "NAME")
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Train train = (Train) o;
+        return Objects.equals(name, train.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    public Train(){
+    }
+}
