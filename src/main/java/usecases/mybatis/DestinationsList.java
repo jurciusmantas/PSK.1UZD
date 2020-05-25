9e61dd0bbf27532c97c12e234f502795b06338c8
jurciusmantas <jurciusmantas@gmail.com>
2020-05-26 01:13:26 +0300

    async loading

--- src/main/java/usecases/mybatis/DestinationsList.java
+++ src/main/java/usecases/mybatis/DestinationsList.java
@@ -1,20 +1,30 @@
 package usecases.mybatis;
 
+import interceptors.Logged;
 import lombok.Getter;
 import lombok.Setter;
 import mybatis.dao.DestinationsMapper;
 import mybatis.model.Destinations;
 
 import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
 import javax.enterprise.inject.Model;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.transaction.Transactional;
+import java.io.IOException;
+import java.io.Serializable;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
-@Model
-public class DestinationsList {
+@Logged
+@Named
+@SessionScoped
+public class DestinationsList implements Serializable {
     @Inject
     private DestinationsMapper destinationsMapper;
+    private CompletableFuture<List<Destinations>> destinationsLoaderTask = null;
 
     @Getter
     private List<Destinations> allDestinations;
@@ -22,13 +32,27 @@ public class DestinationsList {
     @Getter @Setter
     private Destinations destToCreate = new Destinations();
 
-    @PostConstruct
-    public void init() {
-        this.loadAllDestinations();
+
+    public String startLoading(){
+        destinationsLoaderTask = CompletableFuture.supplyAsync(() -> this.loadAllDestinations());
+        return "destinationsListLoader.xhtml?faces-redirect=true";
     }
 
-    private void loadAllDestinations() {
+    public void getDestinationLoaderTaskStatus() throws IOException {
+        if (destinationsLoaderTask == null || destinationsLoaderTask.isDone()){
+            FacesContext.getCurrentInstance().getExternalContext().redirect("destinationsList.xhtml?faces-redirect=true");
+        }
+    }
+
+    private List<Destinations> loadAllDestinations() {
+        try{
+            Thread.sleep(5000);
+        }
+        catch (InterruptedException ignored){
+        }
         this.allDestinations = destinationsMapper.selectAll();
+
+        return this.allDestinations;
     }
 
     @Transactional
